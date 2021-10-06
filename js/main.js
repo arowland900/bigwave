010e3c1d49a753f7e7c2eff3d258d9596e8a58c4
Alexander Rowland <arowland900@gmail.com>
2021-10-06 00:42:20 -0700

    fade adjacent sections on click

--- js/main.js
+++ js/main.js
@@ -3,6 +3,8 @@ let h1 = document.querySelector('h1')
 let img = document.querySelector('img')
 let body = document.querySelector('body')
 let audio = document.getElementById("myAudio")
+let header = document.querySelector("#header")
+let footer = document.querySelector("#tinyFooter")
 
 let fProjexx = document.querySelector('#fullProjexx')
 let mProjexx = document.querySelector('#miniProjexx')
@@ -26,8 +28,8 @@ let three = document.getElementById('three')
 let sections = [true, true, true]
 
 fProjexx.addEventListener('click', swap)
-fSteez.addEventListener('click', swap1)
-fPaul.addEventListener('click', swap2)
+fPaul.addEventListener('click', swap1)
+fSteez.addEventListener('click', swap2)
 audio.loop = true
 
 function swap() {
@@ -35,19 +37,19 @@ function swap() {
     else fadeToggle(mProjexx, fProjexx, 0)
 }
 function swap1() {
-    if (sections[1]) fadeToggle(fSteez, mSteez, 1)
-    else fadeToggle(mSteez, fSteez, 1)
+    if (sections[1]) fadeToggle(fPaul, mPaul, 1)
+    else fadeToggle(mPaul, fPaul, 1)
 }
 function swap2() {
-    if (sections[2]) fadeToggle(fPaul, mPaul, 2)
-    else fadeToggle(mPaul, fPaul, 2)
+    if (sections[2]) fadeToggle(fSteez, mSteez, 2)
+    else fadeToggle(mSteez, fSteez, 2)
 }
 
 function fadeToggle(a, b, c) {
     let s, y, z;
     if (c == 0) { s = swap; y = miniP; z = titleP }
-    else if (c == 1) { s = swap1; y = miniS; z = titleS }
-    else if (c == 2) { s = swap2; y = miniPA; z = titlePA }
+    else if (c == 1) { s = swap1; y = miniPA; z = titlePA }
+    else if (c == 2) { s = swap2; y = miniS; z = titleS }
 
     if (sections[c]) {
         $(a).fadeOut(500, function () {
@@ -146,6 +148,44 @@ $(titleS).click(function () {
 });
 
 function fadeScroll(zone) {
+    console.log("outside: ", sections.toString())
+    if (zone == '#one') {
+        if (sections[1]) {
+            $(fPaul).fadeOut(500)
+            $(fPaul).fadeIn(500)
+        } else {
+            $(mPaul).fadeOut(500)
+            $(mPaul).fadeIn(500)
+        }
+        $(header).fadeOut(500)
+        $(header).fadeIn(500)
+    } else if (zone == '#two') {
+        if (sections[0]){
+            $(fProjexx).fadeOut(500)
+            $(fProjexx).fadeIn(500)            
+        } else {
+            $(mProjexx).fadeOut(500)
+            $(mProjexx).fadeIn(500)
+        }
+        if (sections[2]){
+            $(fSteez).fadeOut(500)
+            $(fSteez).fadeIn(500)
+        } else {
+            $(mSteez).fadeOut(500)
+            $(mSteez).fadeIn(500)
+        }
+    } else {
+        console.log('hit: ', sections)
+        if (sections[1]) {
+            $(fPaul).fadeOut(500)
+            $(fPaul).fadeIn(500)
+        } else {
+            $(mPaul).fadeOut(500)
+            $(mPaul).fadeIn(500)
+        }
+        $(footer).fadeOut(500)
+        $(footer).fadeIn(500)
+    }
     setTimeout(() => {
 
         $('html,body').animate({
@@ -161,19 +201,3 @@ body.style.display = 'none'
 $(body).fadeIn(4000)
 // }
 
-// // call functions here
-// init()
-
-
-// functions
-// function loadHome(){
-//     img.setAttribute('style', 'display: block');
-//     about.setAttribute('style', 'display: none');
-//     contact.setAttribute('style', 'display: none');
-//     music.setAttribute('style', 'display: none');
-//     store.setAttribute('style', 'display: none');
-// }
-
-
-// on page load
-// loadHome()
